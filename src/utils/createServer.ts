ee4b70f087224b51dc47f8fc6e3163aca14d5e75
sergij14 <sergi.jajanidze@gmail.com>
2022-11-27 10:48:35 +0400

    added swagger docs

--- src/utils/createServer.ts
+++ src/utils/createServer.ts
@@ -1,9 +1,28 @@
 import fastify from "fastify";
+import swagger from "@fastify/swagger";
 import { todoRoute } from "../modules/todo/todo-route";
+import { version } from "../../package.json";
 
 export async function createServer() {
   const app = fastify();
 
+  app.register(swagger, {
+    routePrefix: "/docs",
+    swagger: {
+      tags: [
+        {
+          name: "todo",
+        },
+      ],
+      info: {
+        title: "Todo",
+        description: " simple todo app",
+        version,
+      },
+    },
+    staticCSP: true,
+    exposeRoute: true,
+  });
   app.register(todoRoute, { prefix: "/api/v1/todos" });
 
   return app;
